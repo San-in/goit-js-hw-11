69b5959d064932b4066ea250a7d44b1f29fd0b8d
Sanin Oleksandr <saninalexsanin@gmail.com>
2023-02-18 13:31:25 +0200

    init project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import './css/styles.css';
-import handleOnSearch from './handleOnSearch';
-const debounce = require('lodash.debounce');
-
-const DEBOUNCE_DELAY = 300;
-const linkOnSearchForm = document.getElementById('search-box');
-
-linkOnSearchForm.addEventListener('input',debounce(handleOnSearch,DEBOUNCE_DELAY));
 
 
 
