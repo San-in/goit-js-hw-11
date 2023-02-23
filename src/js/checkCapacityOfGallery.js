bd475d488bf4bfc36413b6046f1601dd4753cc09
Sanin Oleksandr <saninalexsanin@gmail.com>
2023-02-23 18:31:08 +0200

    Update checkCapacityOfGallery.js

--- src/js/checkCapacityOfGallery.js
+++ src/js/checkCapacityOfGallery.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export default function checkCapacityOfGallery ({dataOfImages,params,button}) {
-    if (dataOfImages.totalHits - params.showedImages() === 0) {
+    if (dataOfImages.totalHits - params.showedImages() <= 0) {
         Notify.warning('We\'re sorry, but you\'ve reached the end of search results.');
         button.hide();
         return;
