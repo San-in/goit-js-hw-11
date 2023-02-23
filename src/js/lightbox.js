e0535287341f44760062e47708a1ee7f89389a77
Sanin Oleksandr <saninalexsanin@gmail.com>
2023-02-23 18:02:29 +0200

    Update lightbox.js

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-export default lightbox = new SimpleLightbox('.gallery a', {
+export default new SimpleLightbox('.gallery a', {
     captionDelay: 250, 
     });
